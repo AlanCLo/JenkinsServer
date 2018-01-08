54668c1ab491fccd24c7b718ec64a3200d9e0d1e
Alan Lo <lo.alan@gmail.com>
2018-01-08 14:38:57 +1100

    added manual restore script and another demo app

--- backupapps/scripts/restore_app.bash
+++ backupapps/scripts/restore_app.bash
@@ -1,20 +1,20 @@
 #!/bin/bash
 #
-# Creates and Updates a Test database by restoring a copy from daily backup designed for cron script
+# Simple restore that is the counter part of a backup.
+# Only intended as a manual operation.
+# 
+# Run this in parts by sourcing files as needed if you want to do something custom on the cli
 #
 # Arguments:
-#    $1: Name of mirror-profile to source for parameters
+#    $1: Name of profile to source for parameters
 #
 
 
 if [ -z "$1" ]; then
-    echo "Usage: bash mirror_test_db.bash (profile)"
+    echo "Usage: bash restore_app.bash (profile)"
     exit 1
 fi
 source "$1"
-
-# Tell utility.bash that we are mirroring postgresql database here
-export MIRROR_POSTGRESQL="1"
 
 # Safe import of utility module 
 SCRIPT_HOME=$(dirname $(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0))
@@ -29,10 +29,6 @@ fi
 last_backup="$DEST_PREFIX-daily"
 dup_restore "$last_backup" "$RESTORE_LOCATION"
 _exitIfError "Failed to restore last backup at $last_backup"
-
-# Restore database
-database_restore_to_test "$RESTORE_LOCATION"
-_exitIfError "Failed to restore database from file"
 
 # Clean up to avoid leaving environment variables in shell
 config_clear
